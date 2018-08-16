1675fc3c2d76596283ce0d803c6a6ca14bba9be0
AnjirHossain <anjirh@uw.edu>
2018-08-16 00:08:38 -0700

    Post deploy and adding readme

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
-import dotenv from 'dotenv';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './views/styles/index.css';
 import App from './views/containers/App';
 import registerServiceWorker from './views/registerServiceWorker';
-
-dotenv.config();
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
